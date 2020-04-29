e1bae4b84cb39bb9247fbde12250a82de621c370
Colatabajonies <christopher.m.gilmore@gmail.com>
2020-04-29 16:32:27 -0400

    Added ChatStyle static resource

--- .sfdx/typings/lwc/staticresources.d.ts
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -2,3 +2,7 @@ declare module "@salesforce/resourceUrl/CLWCP_Assets" {
     var CLWCP_Assets: string;
     export default CLWCP_Assets;
 }
+declare module "@salesforce/resourceUrl/ChatStyle" {
+    var ChatStyle: string;
+    export default ChatStyle;
+}
